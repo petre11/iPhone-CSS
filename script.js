edfe52f04c87501e0c6f26ff5a28e658ac5b9bba
Piotr Korowicki <Piotrkorowicki@gmail.com>
2017-04-26 22:09:47 +0200

    1 working version

--- script.js
+++ script.js
@@ -1,46 +1,28 @@
+document.addEventListener('DOMContentLoaded', function(event) {
 
-document.addEventListener('DOMContentLoaded',function(event){
-
-var move = document.querySelectorAll('.move');
-  // var moveRight = document.querySelector('.moveRight');
-  // var moveLeft = document.querySelector('.moveLeft');
+  var moveRight = document.querySelector('.moveRight');
+  var moveLeft = document.querySelector('.moveLeft');
   var slide = document.querySelector('.slide');
-    var slideAmount = 4;
+  var slideAmount = 4;
+  var i = 0;
 
   function changeImage(img) {
-        slide.src=img;
+    slide.src = img;
+  }
+  moveRight.addEventListener('click', function(e) {
+    i++;
+    if (i == slideAmount) {
+      i = 0;
     }
-    for (var i=0; i <= move.length; 1++){
-      for(var j = 1; j <= slideAmount; j++){
-        move.addEventListener('click',function(e){
-          if(this.classList('moveRight')== true){
-            changeImage(`galaxy${[j+1]}.jpg`);
-          }
-          else{
-            changeImage(`galaxy${[j-1]}.jpg`);
-          }
-        });
-      }
+    changeImage(`${[i+1]}.jpg`);
+  });
+  moveLeft.addEventListener('click', function() {
+    var i = slide.src.replace('file:///C:/Users/piotr/Desktop/iSlider/', '').replace('.jpg', '')
+    if (i == 1) {
+      i = 5
+      changeImage(`${[i-1]}.jpg`);
     }
-
-
-    // moveRight.addEventListener('click',function(e){
-    //       changeImage(`galaxy${[i+1]}.jpg`);
-    //       i++;
-    //       console.log(i);
-    //       if(i ==slideAmount){
-    //         i=-1;
-    //           i++
-    //       }
-    // });
-    // moveLeft.addEventListener('click',function(){
-    //   changeImage(`galaxy${[i-1]}.jpg`);
-    //   i--;
-    //   if(i ==0){
-    //     i==5;
-    //       i--;
-    //   }
-    //   console.log(i);
-    // });
+    changeImage(`${[i-1]}.jpg`);
+  });
 
 });

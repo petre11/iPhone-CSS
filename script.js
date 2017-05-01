8d493c07b398207c7e7b33b51ed9c67a879ea257
petre11 <piotrkorowicki@gmail.com>
2017-05-01 21:11:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+$(document).ready(function(){
+  var powerButton = $('.button_power');
+  var mainButton = $('.button_main');
+  var logo = $('.logo');
+  var screen = $('.screen');
+  var hamburger = $('.beams');
 
-  var moveRight = document.querySelector('.moveRight');
-  var moveLeft = document.querySelector('.moveLeft');
-  var slide = document.querySelector('.slide');
-  var slideAmount = 4;
-  var i = 0;
-
-  function changeImage(img) {
-    slide.src = img;
-  }
-  moveRight.addEventListener('click', function(e) {
-    i++;
-    if (i == slideAmount) {
-      i = 0;
-    }
-    changeImage(`slide_${[i+1]}.jpg`);
-  });
-  moveLeft.addEventListener('click', function() {
-    var i = slide.src
-    var sliceStart = i.search('slide') + 6;
-    var sliceEnd =  i.search('.jpg');
-    i = i.slice(sliceStart,sliceEnd);
-
-    if (i == 1) {
-      i = 5
-      changeImage(`slide_${[i-1]}.jpg`);
-    }
-    else{
-          changeImage(`slide_${[i-1]}.jpg`);
-    }
-  });
-
+    powerButton.click(function(){
+        mainButton.toggleClass('button_mainOff button_mainOn');
+        logo.toggleClass('logoOn');
+        screen.toggleClass('screenOn');
+        hamburger.toggleClass('beamsOn');
+    });
 });
+
+
+
+
+
+
+
+
+
+//
+//
+// document.addEventListener('DOMContentLoaded', function(event) {
+//
+//   var moveRight = document.querySelector('.moveRight');
+//   var moveLeft = document.querySelector('.moveLeft');
+//   var slide = document.querySelector('.slide');
+//   var slideAmount = 4;
+//   var i = 0;
+//
+//   function changeImage(img) {
+//     slide.src = img;
+//   }
+//   moveRight.addEventListener('click', function(e) {
+//     i++;
+//     if (i == slideAmount) {
+//       i = 0;
+//     }
+//     changeImage(`${[i+1]}.jpg`);
+//   });
+//   moveLeft.addEventListener('click', function() {
+//     var i = slide.src.replace('file:///C:/Users/piotr/Desktop/GitHub/iSlider/', '').replace('.jpg', '')
+//     if (i == 1) {
+//       i = 5
+//       changeImage(`${[i-1]}.jpg`);
+//     }
+//     changeImage(`${[i-1]}.jpg`);
+//   });
+//
+// });
